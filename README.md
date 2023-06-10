7e29b019878ed78b1c352717c1eb4a873956410b
Wentao Cai <chriscwt@outlook.com>
2023-06-10 12:34:45 -0700

    set up repo

--- README.md
+++ README.md
@@ -1,3 +1,165 @@
-# Medley
+# Medley and txMontage
 
-Coming soon.
+Based on the new NonBlock Transaction Composition (NBTC) methodology,
+Medley and txMontage are the two general systems for building
+transactional nonblocking data
+structures for both transient memory and persistent memory,
+respectively. The methodology and the systems are introduced by W.
+Cai, H. Wen, and M. L. Scott from the University of Rochester. The paper appears at SPAA' 23
+([link](https://www.cs.rochester.edu/u/wcai6/papers/2023_Cai_SPAA_NBTC.pdf)).
+
+txMontage is based on
+[nbMontage](https://github.com/urcs-sync/Montage), a fully nonblocking
+variant of Montage that implements periodic persistence, executing all
+cache line writes-back from the same _epoch_ in one batch, therefore
+amortizing the write-back overhead to minimum. It also inherits
+_wait-free_ sync from nbMontage. You may think txMontage as Medley +
+nbMontage.
+
+Due to time constraints during development, Medley and txMontage are
+not well separated and refactored --- they are using the same Montage
+framework. We call the data structures that allocate in DRAM and
+don't use Montage persistence annotation Medley structures, and those
+that allocate in NVM and use Montage persistence annotation txMontage
+structures. Refactoring is WIP: in the ideal case, they should have
+separate base class, `Composable` for Medley structures and
+`Recoverable` for txMontage structures.
+
+The testing harness is based on J. Izraelevitz's
+[parHarness](https://github.com/izrajoe/parHarness). The persistent
+allocator is [Ralloc](https://github.com/urcs-sync/ralloc) from W. Cai et al.
+
+- [Medley and txMontage](#medley-and-txmontage)
+  - [1. Required Libraries](#1-required-libraries)
+  - [2. Commands](#2-commands)
+    - [2.1. Run Specific Test](#21-run-specific-test)
+    - [2.3. Use Medley and txMontage in Your Code](#23-use-medley-and-txmontage-in-your-code)
+    - [2.4. Different Mount Point](#24-different-mount-point)
+  - [3. Static and Dynamic Environment Variables](#3-static-and-dynamic-environment-variables)
+    - [3.1. Static Variables](#31-static-variables)
+    - [3.2. Dynamic Variables](#32-dynamic-variables)
+    - [3.3. Obsolete Variables](#33-obsolete-variables)
+
+## 1. Required Libraries
+
+Source code of most of required libraries are provided in `./ext`,
+which includes
+[Ralloc](https://github.com/urcs-sync/ralloc),
+LFTT ([integrated version](https://github.com/roghnin/tlds) by H. Wen;
+originally forked from [here](https://github.com/ucf-cs/tlds)), and
+TDSL ([integrated
+version](https://github.com/roghnin/TDSL/tree/sl_as_map) by H. Wen;
+originally forked from [here](https://github.com/amitz25/TDSL)).
+
+Other than those in `./ext`, this repository also depends on
+`libhwloc`, `libjemalloc`, `libpthread`, and `libgomp`.
+
+## 2. Commands
+
+First, make sure persistent memory is mounted in DAX mode at
+`/mnt/pmem`. txMontage and persistent OneFile in this
+harness will create heap files prefixed by the user name. Pronto and
+Mnemosyne handle heap files by their own and don't have the user name
+prefix. Please refer to Section [2.3](#23-different-mount-point) if
+NVM is mounted in a different path.
+
+You may run the script from any pwd; it always enters its directory first.
+
+To test scalability:
+
+```bash
+./run.sh
+```
+
+To plot (Rscript for R language required):
+
+```bash
+./data/plot.sh
+```
+
+### 2.1. Run Specific Test
+
+**Please read this subsection if you want to manually test specific
+data structures on some workloads.**
+
+To build harness for testing all the data structures:
+
+```bash
+make
+```
+
+Static variables such as `K_SZ` and `V_SZ` can be set while building,
+to adjust sizes of *string-typed* keys and values on the workloads. See Section
+[3](#3-static-and-dynamic-environment-variables) for more details.
+
+After building, run the following with proper arguments:
+
+```bash
+./bin/main -R <rideable_name> -M <test_mode_name> -t <thread_num> -i <duration_of_some_tests> [-v]
+``` 
+
+Running `./bin/main` without an argument will print help info. Please
+refer to the info for available rideable names and test mode names.
+
+### 2.3. Use Medley and txMontage in Your Code
+
+Medley and txMontage's API can be found in `src/persist/api`.
+Currently, all Montage-based data structures in `src/rideables` are
+using the *Object-oriented* API by deriving from class `Recoverable`.
+Please see data structures `src/rideables/Medley*.hpp` and
+`src/rideables/txMontage*.hpp` as examples of detailed usage.
+
+`atomic_lin_var` is the NBTC version of `std::atomic`. As the key of
+transaction composition, it is defined in `src/persist/EpochSys.hpp`.
+(Sorry for the bad coupling! The project initially wasn't considering
+Medley, the system without persistence, would be useful so until we
+completed the development of txMontage and started doing experiments.
+:P)
+
+### 2.4. Different Mount Point
+
+If NVM is mounted in a different path, please either create a symbolic
+link at `/mnt/pmem` (recommended) or search for `/mnt/pmem` in this
+repository and replace them appeared in the following files:
+
+```bash
+ext/ralloc/src/pm_config.hpp # Ralloc
+src/rideables/OneFile/OneFilePTMLF.hpp # Persistent OneFile
+
+run.sh # script for running harness
+```
+
+## 3. Static and Dynamic Environment Variables
+
+### 3.1. Static Variables
+
+`K_SZ`: Static variable to control key size (Byte) for maps. By
+default it's 32. It needs to be set before compilation to take effect,
+e.g., `K_SZ=40 make`. Don't pass values less than 10!
+
+`V_SZ`: Static variable to control value size (Byte) for maps and
+queues. By default it's 24. It needs to be set before compilation to
+take effect, e.g., `V_SZ=2048 make`.
+
+### 3.2. Dynamic Variables
+
+`prefill`: The number of elements to be prefilled into the tested data
+structure. This variable will overwrite the `prefill` argument passed
+to Test constructors.
+
+`range`: This decides the range of keys in map tests. This variable
+will also overwirte the `range` argument passed to Test constructors.
+
+There are also options mentioned in `./src/persist/README.md` for
+configuring Montage parameter, e.g., epoch length, persisting
+strategy, and buffering container.
+
+### 3.3. Obsolete Variables
+
+There're some obsolete variables no longer useable and are not
+mentioned above. 
+
+To name a few: `KeySize` and `ValueSize`. Inconsistency between them
+and `K_SZ` and `V_SZ` will trigger assertion.
+Please don't use any not mentioned above, unless you know what exactly
+you are doing.
---
+++ run.sh
@@ -0,0 +1,455 @@
+#!/bin/bash
+# THREADS=(1 4 8 12 16 20 24 32 36 40 48 62 72 80 90)
+# VALUE_SIZES=(64 128 256 512 1024 2048 4096)
+
+# go to root (./)
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+outfile_dir="data"
+THREADS=(1 4 8 12 16 20 24 32 36 40 48 62 72 80)
+TASK_LENGTH=30 # length of each workload in second
+REPEAT_NUM=3 # number of trials
+
+# Montage-backed persistent lockfree hashtable that supports transactions
+pm_ht_plain="txMontageLfHashTable<uint64_t>" 
+# Transient lockfree hashtable that supports transactions
+dram_ht_plain=(
+    "MedleyLfHashTable<uint64_t>"
+) 
+# OneFile-backed lockfree hashtable that supports transactions
+of_ht_plain=(
+    "OneFileHashTable<uint64_t>"
+    "POneFileHashTable<uint64_t>"
+) 
+
+# Montage-backed persistent lockfree skiplist that supports transactions
+pm_sl_plain="txMontageFraserSkipList<uint64_t>" 
+# Transient lockfree skiplist that supports transactions
+dram_sl_plain=(
+    "MedleyFraserSkipList<uint64_t>"
+)
+# OneFile-backed lockfree skiplist that supports transactions
+of_sl_plain=(
+    "OneFileSkipList<uint64_t>"
+    "POneFileSkipList<uint64_t>"
+)
+# TDSL-backed lockfree skiplist that supports transactions
+tdsl_sl_plain="TDSLSkipList<uint64_t>"
+# LFTT-backed lockfree skiplist that supports transactions
+lftt_sl_plain="LFTTSkipList<uint64_t>"
+
+# Baseline test without any transaction
+no_map_test_write="TxnMapChurnTest<uint64_t:None>:txn10:g0p0i50rm50:range=1000000:prefill=500000"
+no_map_test_readmost="TxnMapChurnTest<uint64_t:None>:txn10:g90p0i5rm5:range=1000000:prefill=500000"
+no_map_test_5050="TxnMapChurnTest<uint64_t:None>:txn10:g50p0i25rm25:range=1000000:prefill=500000"
+
+# Transaction test for Medley/txMontage
+txmon_map_test_write="TxnMapChurnTest<uint64_t:NBTC>:txn10:g0p0i50rm50:range=1000000:prefill=500000"
+txmon_map_test_readmost="TxnMapChurnTest<uint64_t:NBTC>:txn10:g90p0i5rm5:range=1000000:prefill=500000"
+txmon_map_test_5050="TxnMapChurnTest<uint64_t:NBTC>:txn10:g50p0i25rm25:range=1000000:prefill=500000"
+
+# Transaction test for OneFile
+of_map_test_write="TxnMapChurnTest<uint64_t:OneFile>:txn10:g0p0i50rm50:range=1000000:prefill=500000"
+of_map_test_readmost="TxnMapChurnTest<uint64_t:OneFile>:txn10:g90p0i5rm5:range=1000000:prefill=500000"
+of_map_test_5050="TxnMapChurnTest<uint64_t:OneFile>:txn10:g50p0i25rm25:range=1000000:prefill=500000"
+
+# Transaction test for TDSL
+tdsl_map_test_write="TxnMapChurnTest<uint64_t:TDSL>:txn10:g0p0i50rm50:range=1000000:prefill=500000"
+tdsl_map_test_readmost="TxnMapChurnTest<uint64_t:TDSL>:txn10:g90p0i5rm5:range=1000000:prefill=500000"
+tdsl_map_test_5050="TxnMapChurnTest<uint64_t:TDSL>:txn10:g50p0i25rm25:range=1000000:prefill=500000"
+
+# Transaction test for LFTT
+lftt_map_test_write="TxnMapChurnTest<uint64_t:LFTT>:txn10:g0p0i50rm50:range=1000000:prefill=500000"
+lftt_map_test_readmost="TxnMapChurnTest<uint64_t:LFTT>:txn10:g90p0i5rm5:range=1000000:prefill=500000"
+lftt_map_test_5050="TxnMapChurnTest<uint64_t:LFTT>:txn10:g50p0i25rm25:range=1000000:prefill=500000"
+
+# TPCC test for Medley/txMontage, Onefile, and TDSL
+tpcc_mon="TPCC<NBTC>"
+tpcc_of="TPCC<OneFile>"
+tpcc_tdsl="TPCC<TDSL>"
+
+
+delete_heap_file(){
+    # rm -rf /mnt/pmem/${USER}* /mnt/pmem/savitar.cat /mnt/pmem/psegments
+    # rm -f /mnt/pmem/*.log /mnt/pmem/snapshot*
+    rm -rf /mnt/pmem/*
+    sleep 1
+}
+
+ht_init(){
+    echo "Running hash tables, g0i50r50, g50i25r25 and g90i5r5 for $TASK_LENGTH seconds"
+    rm -rf $outfile_dir/hts_g0i50r50_thread.csv $outfile_dir/hts_g50i25r25_thread.csv $outfile_dir/hts_g90i5r5_thread.csv
+    echo "thread,ops,ds,test" > $outfile_dir/hts_g0i50r50_thread.csv
+    echo "thread,ops,ds,test" > $outfile_dir/hts_g50i25r25_thread.csv
+    echo "thread,ops,ds,test" > $outfile_dir/hts_g90i5r5_thread.csv
+}
+
+ht_execute(){
+    make clean;make -j
+    ht_init
+    # 1. transient that use Montage
+    for ((i=1; i<=REPEAT_NUM; ++i))
+    do
+        for threads in "${THREADS[@]}"
+        do
+            for rideable in "${dram_ht_plain[@]}"
+            do
+                delete_heap_file
+                echo -n "g0i50r50,"
+                ./bin/main -R $rideable -M $txmon_map_test_write -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/hts_g0i50r50_thread.csv
+
+                delete_heap_file
+                echo -n "g50i25r25,"
+                ./bin/main -R $rideable -M $txmon_map_test_5050 -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/hts_g50i25r25_thread.csv
+
+                delete_heap_file
+                echo -n "g90i5r5,"
+                ./bin/main -R $rideable -M $txmon_map_test_readmost -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/hts_g90i5r5_thread.csv
+            done
+        done
+    done
+
+    # 2. Montage with epoch system
+    for ((i=1; i<=REPEAT_NUM; ++i))
+    do
+        for threads in "${THREADS[@]}"
+        do
+            # delete_heap_file
+            # echo -n "g0i50r50,"
+            # ./bin/main -R $pm_ht_plain -M $txmon_map_test_write -t $threads -i $TASK_LENGTH -dLiveness=Blocking | tee -a $outfile_dir/hts_g0i50r50_thread.csv
+
+            # delete_heap_file
+            # echo -n "g50i25r25,"
+            # ./bin/main -R $pm_ht_plain -M $txmon_map_test_5050 -t $threads -i $TASK_LENGTH -dLiveness=Blocking | tee -a $outfile_dir/hts_g50i25r25_thread.csv
+
+            # delete_heap_file
+            # echo -n "g90i5r5,"
+            # ./bin/main -R $pm_ht_plain -M $txmon_map_test_readmost -t $threads -i $TASK_LENGTH -dLiveness=Blocking | tee -a $outfile_dir/hts_g90i5r5_thread.csv
+
+            delete_heap_file
+            echo -n "g0i50r50,"
+            ./bin/main -R $pm_ht_plain -M $txmon_map_test_write -t $threads -i $TASK_LENGTH -dLiveness=Nonblocking | tee -a $outfile_dir/hts_g0i50r50_thread.csv
+
+            delete_heap_file
+            echo -n "g50i25r25,"
+            ./bin/main -R $pm_ht_plain -M $txmon_map_test_5050 -t $threads -i $TASK_LENGTH -dLiveness=Nonblocking | tee -a $outfile_dir/hts_g50i25r25_thread.csv
+
+            delete_heap_file
+            echo -n "g90i5r5,"
+            ./bin/main -R $pm_ht_plain -M $txmon_map_test_readmost -t $threads -i $TASK_LENGTH -dLiveness=Nonblocking | tee -a $outfile_dir/hts_g90i5r5_thread.csv
+        done
+    done
+
+    # 3. OneFile
+    for ((i=1; i<=REPEAT_NUM; ++i))
+    do
+        for threads in "${THREADS[@]}"
+        do
+            for rideable in "${of_ht_plain[@]}"
+            do
+                delete_heap_file
+                echo -n "g0i50r50,"
+                ./bin/main -R $rideable -M $of_map_test_write -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/hts_g0i50r50_thread.csv
+
+                delete_heap_file
+                echo -n "g50i25r25,"
+                ./bin/main -R $rideable -M $of_map_test_5050 -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/hts_g50i25r25_thread.csv
+
+                delete_heap_file
+                echo -n "g90i5r5,"
+                ./bin/main -R $rideable -M $of_map_test_readmost -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/hts_g90i5r5_thread.csv
+            done
+        done
+    done
+}
+
+
+sl_init(){
+    echo "Running skiplists, g0i50r50, g50i25r25 and g90i5r5 for $TASK_LENGTH seconds"
+    rm -rf $outfile_dir/sls_g0i50r50_thread.csv $outfile_dir/sls_g50i25r25_thread.csv $outfile_dir/sls_g90i5r5_thread.csv
+    echo "thread,ops,ds,test" > $outfile_dir/sls_g0i50r50_thread.csv
+    echo "thread,ops,ds,test" > $outfile_dir/sls_g50i25r25_thread.csv
+    echo "thread,ops,ds,test" > $outfile_dir/sls_g90i5r5_thread.csv
+}
+
+sl_execute(){
+    make clean;make -j
+    sl_init
+    # 1. transient that use Montage
+    for ((i=1; i<=REPEAT_NUM; ++i))
+    do
+        for threads in "${THREADS[@]}"
+        do
+            for rideable in "${dram_sl_plain[@]}"
+            do
+                delete_heap_file
+                echo -n "g0i50r50,"
+                ./bin/main -R $rideable -M $txmon_map_test_write -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_g0i50r50_thread.csv
+
+                delete_heap_file
+                echo -n "g50i25r25,"
+                ./bin/main -R $rideable -M $txmon_map_test_5050 -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_g50i25r25_thread.csv
+
+                delete_heap_file
+                echo -n "g90i5r5,"
+                ./bin/main -R $rideable -M $txmon_map_test_readmost -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_g90i5r5_thread.csv
+            done
+        done
+    done
+
+    # 2. Montage with epoch system
+    for ((i=1; i<=REPEAT_NUM; ++i))
+    do
+        for threads in "${THREADS[@]}"
+        do
+            # delete_heap_file
+            # echo -n "g0i50r50,"
+            # ./bin/main -R $pm_sl_plain -M $txmon_map_test_write -t $threads -i $TASK_LENGTH -dLiveness=Blocking | tee -a $outfile_dir/sls_g0i50r50_thread.csv
+
+            # delete_heap_file
+            # echo -n "g50i25r25,"
+            # ./bin/main -R $pm_sl_plain -M $txmon_map_test_5050 -t $threads -i $TASK_LENGTH -dLiveness=Blocking | tee -a $outfile_dir/sls_g50i25r25_thread.csv
+
+            # delete_heap_file
+            # echo -n "g90i5r5,"
+            # ./bin/main -R $pm_sl_plain -M $txmon_map_test_readmost -t $threads -i $TASK_LENGTH -dLiveness=Blocking | tee -a $outfile_dir/sls_g90i5r5_thread.csv
+
+            delete_heap_file
+            echo -n "g0i50r50,"
+            ./bin/main -R $pm_sl_plain -M $txmon_map_test_write -t $threads -i $TASK_LENGTH -dLiveness=Nonblocking | tee -a $outfile_dir/sls_g0i50r50_thread.csv
+
+            delete_heap_file
+            echo -n "g50i25r25,"
+            ./bin/main -R $pm_sl_plain -M $txmon_map_test_5050 -t $threads -i $TASK_LENGTH -dLiveness=Nonblocking | tee -a $outfile_dir/sls_g50i25r25_thread.csv
+
+            delete_heap_file
+            echo -n "g90i5r5,"
+            ./bin/main -R $pm_sl_plain -M $txmon_map_test_readmost -t $threads -i $TASK_LENGTH -dLiveness=Nonblocking | tee -a $outfile_dir/sls_g90i5r5_thread.csv
+        done
+    done
+
+    # 3. OneFile
+    for ((i=1; i<=REPEAT_NUM; ++i))
+    do
+        for threads in "${THREADS[@]}"
+        do
+            for rideable in "${of_sl_plain[@]}"
+            do
+                delete_heap_file
+                echo -n "g0i50r50,"
+                ./bin/main -R $rideable -M $of_map_test_write -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_g0i50r50_thread.csv
+
+                delete_heap_file
+                echo -n "g50i25r25,"
+                ./bin/main -R $rideable -M $of_map_test_5050 -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_g50i25r25_thread.csv
+
+                delete_heap_file
+                echo -n "g90i5r5,"
+                ./bin/main -R $rideable -M $of_map_test_readmost -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_g90i5r5_thread.csv
+            done
+        done
+    done
+
+    # 4. TDSL
+    for ((i=1; i<=REPEAT_NUM; ++i))
+    do
+        for threads in "${THREADS[@]}"
+        do
+            delete_heap_file
+            echo -n "g0i50r50,"
+            ./bin/main -R $tdsl_sl_plain -M $tdsl_map_test_write -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_g0i50r50_thread.csv
+
+            delete_heap_file
+            echo -n "g50i25r25,"
+            ./bin/main -R $tdsl_sl_plain -M $tdsl_map_test_5050 -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_g50i25r25_thread.csv
+
+            delete_heap_file
+            echo -n "g90i5r5,"
+            ./bin/main -R $tdsl_sl_plain -M $tdsl_map_test_readmost -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_g90i5r5_thread.csv
+        done
+    done
+
+    # 5. LFTT
+    ### Integrating LFTT into our testing framework results in slow
+    ### performance. We can't figure out why and which side the bug is
+    ### on, so we instead run it with its native testing framework for fairness.
+    ext/lftt/txn_run.sh
+}
+
+tpcc_init(){
+    echo "Running tpcc on hash tables for $TASK_LENGTH seconds"
+    rm -rf $outfile_dir/tpcc.csv
+    echo "thread,ops,ds,test" > $outfile_dir/tpcc.csv
+}
+
+tpcc_execute(){
+    tpcc_init
+    for ((i=1; i<=REPEAT_NUM; ++i))
+    do
+        make clean;make medley_tpcc -j
+        for threads in "${THREADS[@]}"
+        do
+            delete_heap_file
+            ./bin/main -R ${dram_sl_plain[0]} -M $tpcc_mon -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/tpcc.csv
+
+        done
+        
+        make clean;make txmon_tpcc -j
+        for threads in "${THREADS[@]}"
+        do
+            delete_heap_file
+            ./bin/main -R $pm_sl_plain -M $tpcc_mon -t $threads -dLiveness=Nonblocking -i $TASK_LENGTH | tee -a $outfile_dir/tpcc.csv
+        done
+        
+        make clean;make of_tpcc -j
+        for threads in "${THREADS[@]}"
+        do
+            delete_heap_file
+            ./bin/main -R ${of_sl_plain[0]} -M $tpcc_of -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/tpcc.csv
+        done
+        
+        ### Too slow to finish, skipping
+        # make clean;make pof_tpcc -j
+        # for threads in "${THREADS[@]}"
+        # do
+        #     delete_heap_file
+        #     ./bin/main -R ${of_sl_plain[1]} -M $tpcc_of -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/tpcc.csv
+        # done
+
+        make clean;make tdsl_tpcc -j
+        for threads in "${THREADS[@]}"
+        do
+            delete_heap_file
+            ./bin/main -R $tdsl_sl_plain -M $tpcc_tdsl -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/tpcc.csv
+        done
+    done
+}
+
+
+sls_latency_init(){
+    echo "Running latency on skiplist for $TASK_LENGTH seconds"
+    rm -rf $outfile_dir/sls_latency.csv
+    echo "thread,ops,ds,test" > $outfile_dir/sls_latency.csv
+}
+
+sls_latency_execute(){
+    sls_latency_init
+    threads=40
+
+    make clean;make -j
+    for ((i=1; i<=REPEAT_NUM; ++i))
+    do
+        # Original DRAM
+        delete_heap_file
+        echo -n "g0i50r50,"
+        ./bin/main -R "FraserSkipList<uint64_t>" -M $no_map_test_write -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g50i25r25,"
+        ./bin/main -R "FraserSkipList<uint64_t>" -M $no_map_test_5050 -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g90i5r5,"
+        ./bin/main -R "FraserSkipList<uint64_t>" -M $no_map_test_readmost -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        # Original NVM
+        delete_heap_file
+        echo -n "g0i50r50,"
+        ./bin/main -R "NVMFraserSkipList<uint64_t>" -M $no_map_test_write -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g50i25r25,"
+        ./bin/main -R "NVMFraserSkipList<uint64_t>" -M $no_map_test_5050 -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g90i5r5,"
+        ./bin/main -R "NVMFraserSkipList<uint64_t>" -M $no_map_test_readmost -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        # Medley no txn
+        delete_heap_file
+        echo -n "g0i50r50,"
+        ./bin/main -R ${dram_sl_plain[0]} -M $no_map_test_write -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g50i25r25,"
+        ./bin/main -R ${dram_sl_plain[0]} -M $no_map_test_5050 -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g90i5r5,"
+        ./bin/main -R ${dram_sl_plain[0]} -M $no_map_test_readmost -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        # Montage no txn no persist
+        delete_heap_file
+        echo -n "g0i50r50,"
+        ./bin/main -R $pm_sl_plain -M $no_map_test_write -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g50i25r25,"
+        ./bin/main -R $pm_sl_plain -M $no_map_test_5050 -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g90i5r5,"
+        ./bin/main -R $pm_sl_plain -M $no_map_test_readmost -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        # Montage no txn persist
+        delete_heap_file
+        echo -n "g0i50r50,"
+        ./bin/main -R $pm_sl_plain -M $no_map_test_write -t $threads -dLiveness=Nonblocking -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g50i25r25,"
+        ./bin/main -R $pm_sl_plain -M $no_map_test_5050 -t $threads -dLiveness=Nonblocking -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g90i5r5,"
+        ./bin/main -R $pm_sl_plain -M $no_map_test_readmost -t $threads -dLiveness=Nonblocking -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        # Medley txn
+        delete_heap_file
+        echo -n "g0i50r50,"
+        ./bin/main -R ${dram_sl_plain[0]} -M $txmon_map_test_write -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g50i25r25,"
+        ./bin/main -R ${dram_sl_plain[0]} -M $txmon_map_test_5050 -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g90i5r5,"
+        ./bin/main -R ${dram_sl_plain[0]} -M $txmon_map_test_readmost -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        # Montage txn no persist
+        delete_heap_file
+        echo -n "g0i50r50,"
+        ./bin/main -R $pm_sl_plain -M $txmon_map_test_write -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g50i25r25,"
+        ./bin/main -R $pm_sl_plain -M $txmon_map_test_5050 -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g90i5r5,"
+        ./bin/main -R $pm_sl_plain -M $txmon_map_test_readmost -t $threads -dPersistStrat=No -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        # Montage txn persist
+        delete_heap_file
+        echo -n "g0i50r50,"
+        ./bin/main -R $pm_sl_plain -M $txmon_map_test_write -t $threads -dLiveness=Nonblocking -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g50i25r25,"
+        ./bin/main -R $pm_sl_plain -M $txmon_map_test_5050 -t $threads -dLiveness=Nonblocking -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+
+        delete_heap_file
+        echo -n "g90i5r5,"
+        ./bin/main -R $pm_sl_plain -M $txmon_map_test_readmost -t $threads -dLiveness=Nonblocking -i $TASK_LENGTH | tee -a $outfile_dir/sls_latency.csv
+    done
+}
+
+
+########################
+###       Main       ###
+########################
+ht_execute
+sl_execute
+tpcc_execute
+sls_latency_execute
